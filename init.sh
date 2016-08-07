5ed3302f65872b708b790e523afa8b47972ea3e6
Victor Melnyk <victor.melnyk.vm@gmail.com>
2016-08-07 03:45:43 +0300

    add init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+# create new password for root
+
+# add user (from root)
+# adduser uadmin
+#   # enter data
+# usermod -aG sudo uadmin
+
+# update (from uadmin with sudo)
+apt-get update
+apt-get upgrade
