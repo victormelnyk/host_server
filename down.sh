0c25e9bd83f15474f73d4a4cc257ce37c1d3ac2a
Victor Melnyk <victor.melnyk.vm@gmail.com>
2016-12-25 19:37:11 +0200

    update dc files

--- down.sh
+++ down.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 docker-compose \
-  -f dc/sys/nginx.yml \
   -f dc/sys/postgres.yml \
-  -f dc/sys/jenkins.yml \
   -f dc/app/test_app.yml \
-  -f dc/app/like-stat.yml \
   down
+#  -f dc/sys/jenkins.yml \
+#  -f dc/app/like-stat.yml \
+#  -f dc/sys/nginx.yml \
+#  -f dc/sys/log.yml \
